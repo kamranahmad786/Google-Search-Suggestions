898c6f0787beeb6603855d43e88cd7965a524624
kamran <mohammadkamranahmad786@gmail.com>
2024-03-22 23:59:07 +0000

    Solution

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,31 @@
+// Write your code here
+
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
